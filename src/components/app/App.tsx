060d258cafc4471fc3d0ba474c2ffc7427f92b88
Maksim Fedorenkov <maksim.fedorenkov@rtkit.ru>
2023-03-06 17:14:38 +0300

    sprint-1/step-1 BurgerIngredients готов компонент

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className={styles.app}>
       <AppHeader />
-      <main>
+      <main className={styles.main}>
         <BurgerIngredients />
       </main>
     </div>
--- src/components/burgerIngredients/components/cardElement.tsx
+++ src/components/burgerIngredients/components/cardElement.tsx
@@ -21,29 +21,47 @@ interface IData {
   v?: number
 }
 
+const categoryTypes = {
+  bun: "bun",
+  main: "main",
+  sauce: "sauce"
+}
+
 export const CardElement = ({
   data = []
 }:TCardElement): JSX.Element => {
 
-  const renderListData = (data: IData[]) => {
+  const renderListData = (data: IData[], categoryTypesElement: string) => {
     return (
       data.map((el) => (
-          <div className={`${styles["cart"]}`}>
-            <img src={el.image} alt="bun" />
-            <div>
-              <span className={`${styles["price"]} text_type_main-default`}>{el.price}</span>
-              <CurrencyIcon type={'secondary'} />
-            </div>
-            <span>{el.name}</span>
+        el.type === categoryTypesElement && (
+        <div className={`${styles["cart"]}`}>
+          <img src={el.image} alt="bun" className='ml-4 mr-4'/>
+          <div>
+            <div className={`${styles["price"]} text_type_digits-default mt-1 mb-1`}>{el.price}<CurrencyIcon type={'secondary'} /></div>
           </div>
+          <span className={`${styles["description"]} text_type_main-default`}text_type_main-default >{el.name}</span>
+        </div>)
         )
       )
     )
   }
 
   return (
-    <>
-      {renderListData(data)}
-    </>
+    <div className={`${styles["category-wraper"]} custom-scroll`}>
+      <div>
+        <h3 className={`${styles["category-title"]}`}>Булки</h3>
+        <div className={`${styles["category-list"]}`}>{renderListData(data, categoryTypes.bun)}</div>
+      </div>
+      <div>
+        <h3 className={`${styles["category-title"]}`}>Соусы</h3>
+        <div className={`${styles["category-list"]}`}>{renderListData(data, categoryTypes.sauce)}</div>
+      </div>
+      
+      <div>
+        <h3 className={`${styles["category-title"]}`}>Главное</h3>
+        <div className={`${styles["category-list"]}`}>{renderListData(data, categoryTypes.main)}</div>
+      </div>
+    </div>
   )
 }
