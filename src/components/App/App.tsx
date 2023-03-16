45dbfdcaa93c4664ca192ae2ba705975527ff39e
Maksim Fedorenkov <maksim.fedorenkov@rtkit.ru>
2023-03-16 10:07:46 +0300

    sprint-2/step-1 переписал хранилище с useState на useReducer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { AppHeader } from '../AppHeader/AppHeader';
 import { BurgerConstructor } from '../BurgerConstructor/BurgerConstructor';
 import { BurgerIngredients } from '../BurgerIngredients/BurgerIngredients';
+import { DataBurgerConstructor } from '../../services/appContext'
 import styles from './App.module.css';
 
 function App() {
 
   const URL = "https://norma.nomoreparties.space/api/ingredients ";
 
-  const [data, setData] = React.useState({
+  const initialState = { 
     success: false,
-    data: []}
-  )
+    data: []
+  } 
+
+  function reducer(state: any, action: { type: any; payload: any; } | undefined) {
+    switch (action?.type) {
+      case "set":
+        return { success: action?.payload.success, data: action?.payload.data };
+      case "reset":
+        return initialState 
+      default:
+        throw new Error(`Wrong type of action: ${action?.type}`);
+    }
+  }
 
   React.useEffect(() => {
     fetch(URL)
@@ -21,18 +33,23 @@ function App() {
         }
         return Promise.reject(`Ошибка ${res.status}`);
       })
-      .then(json => setData(json))
+      .then(json => dataStateDispatcher({type: 'set', payload: json}))
       .catch((error) => console.error(`"Что то пошло не так", ${error}`))
   }, []);
+
+  const [dataState, dataStateDispatcher] = React.useReducer(reducer, initialState, undefined);
+  React.useEffect(() => {console.log(dataState)},[dataState])
 
   return (
     <div className={styles.app}>
       <AppHeader />
       <main className={styles.main}>
-        {data.success && (
+        {dataState.success && (
           <>
-            <BurgerIngredients data={data.data}/>
-            <BurgerConstructor data={data.data}/>
+            <DataBurgerConstructor.Provider value={{ dataState, dataStateDispatcher }}>
+              <BurgerIngredients data={dataState.data}/>
+              <BurgerConstructor data={dataState.data}/>
+            </DataBurgerConstructor.Provider>
           </>
         )}
       </main>
